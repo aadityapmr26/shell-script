dac0ef2ce145117dd75151da990436461cebe1db
Pokkunuri <adityapi@amazon.com>
2025-01-04 22:37:00 +0530

    variables

---
+++ 04.variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1 #nospacebefore and after equal to
+PERSON2=$2
+
+
+echo "$PERSON1:: HI $PERSON2"
+echo "$PERSON2:: Hello $PERSON1"
+echo "$PERSON1:: How are you doing"
+echo "$PERSON2:: I am Good, How are you?"
