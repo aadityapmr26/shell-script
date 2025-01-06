d0e43a8ef5c4d929f3decaa3f232816e5dedcd1d
Pokkunuri <adityapi@amazon.com>
2025-01-06 12:07:07 +0530

    Datatype

---
+++ 08-special-vars.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+echo "All Variables passed: $@"
+echo "Number of variables : $#"
+echo "Current working Directory: $PWD"
+echo "Script Name: $0"
+echo "Home Directory of  curr user: $HOME"
+echo "Current user executing the script $USER"
+echo "Current background process $!"
+echo "Process id of  current script $$"
