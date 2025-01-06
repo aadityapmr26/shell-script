c40a106df97955982f836d422bd42ebcfd021193
Pokkunuri <adityapi@amazon.com>
2025-01-06 11:54:16 +0530

    Datatype

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -6,5 +6,5 @@ echo "First Movie is ${MOVIES[0]}"
 echo "Second Movie is ${MOVIES[1]}"
 echo "Third Movie is ${MOVIES[2]}"
 
-echo "All Movies are ${Movies[@]}"
+echo "All Movies are ${MOVIES[@]}"
 
