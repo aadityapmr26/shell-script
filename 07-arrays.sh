a39f0aa51c595d52bcbadab466586553405d60a8
Pokkunuri <adityapi@amazon.com>
2025-01-06 11:50:24 +0530

    Datatype

---
+++ 07-arrays.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MOVIES=("Blood Diamond","House of Wax","Final Destination")
+
+echo "First Movie is ${MOVIES[0]}"
+echo "Second Movie is ${MOVIES[1]}"
+echo "Third Movie is ${MOVIES[2]}"
+
+echi "All Movies are ${Movies[@]}"
+
