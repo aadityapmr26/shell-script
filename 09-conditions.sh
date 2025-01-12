07e73209a5a4494cca55bcf26298a052cc3e61d4
Pokkunuri <adityapi@amazon.com>
2025-01-12 10:52:38 +0530

    conditions

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -4,7 +4,7 @@ NUMBER = $1
 
 if [ $NUMBER -gt 100]
 then
-echo "The $NUMBER is greater than 100"
+    echo "The $NUMBER is greater than 100"
 else
-echo "The $NUMBER is less than 100"
+     echo "The $NUMBER is less than or equal to 100"
 fi
